6cc3e8b183c1e5af0850a04badb8246e9da17a81
Tanmay Khandelwal <58903502+Dude-901@users.noreply.github.com>
2020-08-26 22:56:23 +0530

    pixel color and coordinates on mouse click

---
+++ img_info.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+
+
+def click_event(event, x, y, flags, params):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        print(x, ' ', y)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        strXY = str(x) + ' ' + str(y)
+        cv2.putText(img, strXY, (x,y), font, 0.5, (105,255,0), 2)
+        cv2.imshow('image', img)
+    if event == cv2.EVENT_RBUTTONDOWN:
+        blue = img[x, y, 0]
+        red = img[x, y, 1]
+        green = img[x, y, 2]
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        strBGR = str(blue) + ' ' + str(green) + ' ' + str(red)
+        cv2.putText(img, strBGR, (x,y), font, 0.5, (255,0,105), 2)
+        cv2.imshow('image', img)
+
+
+img = cv2.imread('lena.jpg')
+# img = np.zeros((512,512,3), np.uint8)
+# black image
+cv2.imshow('image', img)
+
+cv2.setMouseCallback('image', click_event)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
