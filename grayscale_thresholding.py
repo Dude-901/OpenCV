9c52403254599195a2f8283e122a5efb2446f2a8
Tanmay Khandelwal <58903502+Dude-901@users.noreply.github.com>
2020-08-31 12:20:51 +0530

    Update grayscale_thresholding.py

--- grayscale_thresholding.py
+++ grayscale_thresholding.py
@@ -9,12 +9,19 @@ _, th4 = cv.threshold(img, 127, 255, cv.THRESH_TOZERO)
 _, th5 = cv.threshold(img, 127, 255, cv.THRESH_TOZERO_INV)
 
 
-cv.imshow("Image", img)
-cv.imshow("th1", th1)
-cv.imshow("th2", th2)
-cv.imshow("th3", th3)
-cv.imshow("th4", th4)
-cv.imshow("th5", th5)
+titles = ['Original Image','BINARY','BINARY_INV','TRUNC','TOZERO','TOZERO_INV']
+images = [img, th1 ,th2 ,th3 ,th4, th5]
 
-cv.waitKey(0)
-cv.destroyAllWindows()
+for i in range(6):
+    plt.subplot(2, 3, i+1), plt.imshow(images[i], 'gray')
+    plt.title(titles[i])
+    plt.xticks([]),plt.yticks([])
+#cv.imshow("Image", img)
+#cv.imshow("th1", th1)
+#cv.imshow("th2", th2)
+#cv.imshow("th3", th3)
+#cv.imshow("th4", th4)
+#cv.imshow("th5", th5)
+plt.show()
+#cv.waitKey(0)
+#cv.destroyAllWindows()
